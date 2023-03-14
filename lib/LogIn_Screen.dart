9e6e7f06f69a69b31c531b09b6f6ca9d2f16d04c
Abdallah-S-Abdallah <abdallahsalama808@gmail.com>
2023-03-14 20:44:24 +0200

    Login Screen

---
+++ lib/LogIn_Screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LogIn_Screen extends StatelessWidget{
+  var student_Code = TextEditingController();
+  var pass_Code = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.menu,
+          ),
+        ),
+        title: const Text(
+          'Account Login',
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 50.0,
+            ),
+            //Student Code
+            TextFormField(
+              controller: student_Code,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                  ),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                  ),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                labelText: 'Student Code',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(
+                  Icons.account_box,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            //Pass Code
+            TextFormField(
+              controller: pass_Code,
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                  ),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                  ),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                labelText: 'Pass Code',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(
+                  Icons.lock,
+                ),
+                suffixIcon: Icon(
+                  Icons.remove_red_eye,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            //LogIn
+            Container(
+              width: double.infinity,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              child: MaterialButton(
+                onPressed: () {
+                  print(student_Code.text);
+                  print(pass_Code.text);
+                },
+                color: Colors.black87,
+                height: 50.0,
+                child: const Text(
+                  'LogIn',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
